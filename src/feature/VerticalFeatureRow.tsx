e0b1329ffb0e42c80166ac26005915e77037fec6
victorpb96 <120343714+victorpb96@users.noreply.github.com>
2024-06-22 00:53:51 +0800

    rounded-lg added finally

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -30,7 +30,11 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
       </div>
 
       <div className="w-full rounded-3xl p-6 sm:w-1/2">
-        <img src={`${router.basePath}${props.image}`} alt={props.imageAlt} />
+        <img
+          src={`${router.basePath}${props.image}`}
+          alt={props.imageAlt}
+          className="rounded-lg"
+        />
       </div>
     </div>
   );
--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -5,7 +5,7 @@ const Sponsors = () => (
     title="utilise NDIS funding to receive care"
     description="Choose your support worker from our database of local support workers"
     // eslint-disable-next-line react/no-children-prop
-    children="#"
+    children=""
   ></Section>
 );
 
